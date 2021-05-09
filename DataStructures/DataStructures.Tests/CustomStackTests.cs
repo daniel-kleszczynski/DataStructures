576c7fb94fa2de337f409fc855443b7e4119350f
daniel-kleszczynski <danielkleszczynski@gmail.com>
2021-05-09 20:55:02 +0200

    Create  unit tests project for UI and rename unit tests project for Lib

--- DataStructures/DataStructures.Tests/CustomStackTests.cs
+++ DataStructures/DataStructures.Tests/CustomStackTests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 using System;
 using System.Linq;
 
-namespace DataStructures.Tests
+namespace DataStructures.LibTests
 {
     [TestFixture]
     public class CustomStackTests
