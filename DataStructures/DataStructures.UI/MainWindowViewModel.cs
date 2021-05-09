75efa3a599b460917c76cdf1b4492cc87988b97a
daniel-kleszczynski <danielkleszczynski@gmail.com>
2021-05-09 18:09:25 +0200

    Initial MVVM setup

---
+++ DataStructures/DataStructures.UI/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.UI
+{
+    public class MainWindowViewModel
+    {
+        public string ExampleText => "Some text";
+    }
+}
