8c9f74672b8aaa5ab088482a529864f8107ca9e7
daniel-kleszczynski <danielkleszczynski@gmail.com>
2021-05-16 20:14:28 +0200

    Add Custom ICommand implementation

---
+++ DataStructures/DataStructures.UI/Command/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DataStructures.UI.Command
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_execute == null || (_canExecute != null && _canExecute.Invoke(parameter) == false))
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
---
+++ DataStructures/DataStructures.UiTests/Command/RelayCommandTests.cs
@@ -0,0 +1,70 @@
+﻿using DataStructures.UI.Command;
+using NUnit.Framework;
+using System;
+
+namespace DataStructures.UiTests.Command
+{
+    [TestFixture]
+    public class RelayCommandTests
+    {
+        [Test]
+        public void CanExecute_ExecuteDelegateIsNull_ReturnsFalse()
+        {
+            //Arrange
+            Action<object> execute = null;
+            Func<object, bool> canExecute = (o) => true;
+            RelayCommand command = new RelayCommand(execute, canExecute);
+
+            //Act
+            bool result = command.CanExecute(null);
+
+            //Assert
+            Assert.That(result, Is.False);
+;        }
+
+        [Test]
+        public void CanExecute_CanExecuteDelegateIsNull_ReturnsTrue()
+        {
+            //Arrange
+            Action<object> execute = (o) => { };
+            Func<object, bool> canExecute = null;
+            RelayCommand command = new RelayCommand(execute, canExecute);
+
+            //Act
+            bool result = command.CanExecute(null);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanExecute_CanExecuteDelegateReturnsFalse_ReturnsFalse()
+        {
+            //Arrange
+            Action<object> execute = (o) => { };
+            Func<object, bool> canExecute = (o) => false;
+            RelayCommand command = new RelayCommand(execute, canExecute);
+
+            //Act
+            bool result = command.CanExecute(null);
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CanExecute_CanExecuteDelegateReturnsTrue_ReturnsTrue()
+        {
+            //Arrange
+            Action<object> execute = (o) => { };
+            Func<object, bool> canExecute = (o) => true;
+            RelayCommand command = new RelayCommand(execute, canExecute);
+
+            //Act
+            bool result = command.CanExecute(null);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
